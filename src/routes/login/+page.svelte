89342ea77533ae29aece239e635b0fbf37d23b8c
Jonas Bugelnig <115107409+jbugel-lol@users.noreply.github.com>
2024-01-28 00:47:57 +0100

    login page improvements

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,16 +1,17 @@
 <script>
   import { goto } from "$app/navigation";
   import Modal from "$lib/components/Modal.svelte";
-  let wrongPassword = false;
 
+  let wrongPassword = false;
   let password = "";
+
   async function submit() {
-    const request = await fetch("/login", {
+    const response = await fetch("/login", {
       method: "POST",
       body: JSON.stringify({ password }),
     });
 
-    const { success } = await request.json();
+    const { success } = await response.json();
 
     if (success) {
       goto("/admin");
@@ -24,21 +25,26 @@
 <div class="bg-cat-mantle min-h-screen text-cat-subtext1">
   <div class="lg:w-1/4 w-11/12 mx-auto flex flex-col gap-2">
     <h1 class="font-semibold text-xl mt-8">Link Control Panel</h1>
-    <input class="rounded-lg p-2 bg-cat-crust placeholder:text-cat-text" bind:value={password} placeholder="Password" type="password" id="Password" name="Password" />
+    <input
+      class="rounded-lg p-2 bg-cat-crust placeholder:text-cat-text"
+      bind:value={password}
+      placeholder="Password"
+      type="password"
+      id="Password"
+      name="Password"
+    />
     <button
-      on:click={() => {
-        submit();
-      }}
+      on:click={submit}
       class="bg-cat-green text-cat-crust rounded-lg p-2 font-bold mt-4"
       type="button"
-      value="Submit"
-      id="button">login</button
+      id="button">Login</button
     >
 
     <Modal bind:showModal={wrongPassword}>
-    <p slot="header" class="text-3xl font-bold">Incorrect Password</p>
+      <p slot="header" class="text-3xl font-bold">Incorrect Password</p>
       <div class="flex flex-col gap-3 mb-8">
         Please enter the correct password and try again.
+      </div>
     </Modal>
   </div>
 </div>
