0d5daf3a1414ee0d4b79e6a113e6f59298d4616c
Jonas Bugelnig <115107409+jbugel-lol@users.noreply.github.com>
2024-01-28 00:10:08 +0100

    Added a modal popup when entering wrong password

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,7 @@
 <script>
   import { goto } from "$app/navigation";
+  import Modal from "$lib/components/Modal.svelte";
+  let wrongPassword = false;
 
   let password = "";
   async function submit() {
@@ -14,7 +16,7 @@
       goto("/admin");
     } else {
       password = "";
-      alert("Incorrect Admin Password");
+      wrongPassword = true;
     }
   }
 </script>
@@ -22,19 +24,7 @@
 <div class="bg-cat-mantle min-h-screen text-cat-subtext1">
   <div class="lg:w-1/4 w-11/12 mx-auto flex flex-col gap-2">
     <h1 class="font-semibold text-xl mt-8">Link Control Panel</h1>
-    <!--<label for="Passord">Password:</label>-->
     <input class="rounded-lg p-2 bg-cat-crust placeholder:text-cat-text" bind:value={password} placeholder="Password" type="password" id="Password" name="Password" />
-    <!--<label for="OTP">OTP:</label><br />
-            <input
-                type="text"
-                inputmode="numeric"
-                pattern="[0-9]*"
-                autocomplete="one-time-code"
-                maxlength="6"
-                placeholder="2FA-Token"
-                size="6"
-                oninput="this.value = this.value.replace(/[^0-9]/g, '');"
-            /> -->
     <button
       on:click={() => {
         submit();
@@ -44,5 +34,11 @@
       value="Submit"
       id="button">login</button
     >
+
+    <Modal bind:showModal={wrongPassword}>
+    <p slot="header" class="text-3xl font-bold">Incorrect Password</p>
+      <div class="flex flex-col gap-3 mb-8">
+        Please enter the correct password and try again.
+    </Modal>
   </div>
 </div>
