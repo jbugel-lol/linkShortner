9d6ff9e7caa682e9367e26311e0b1ab0f7a7ea84
jbugel-lol <jonasbugelnig+gh1@jbugel.xyz>
2024-03-02 23:43:29 +0100

    wrong class

--- src/lib/components/CountryView.svelte
+++ src/lib/components/CountryView.svelte
@@ -9,9 +9,9 @@
     }
 </script>
 
-<div class="hidden group-hover:flex hover:flex flex-col gap-4 absolute top-10 right-0 w-max bg-cat-surface0 p-4 rounded-xl">
+<div class="hidden group-hover:flex hover:flex flex-col gap-4 absolute top-10 right-0 w-max bg-cat-surface0 p-4 rounded-xl z-10">
     {#each countryArray as country}
-        <div class="flex gap-1 z-10">
+        <div class="flex gap-1">
             <img class="rounded-xl" src="https://flagsapi.com/{country.country == 'XX' ? 'US' : country.country}/flat/24.png" alt="" />
             <p>{country.country == "XX" ? "XX" : country.country}: {country.clicks}</p>
         </div>
